169ea7d16cb87c74d804148fbc5274198126fade
Theo LEBRUN <mib.ftw@gmail.com>
2022-09-25 11:06:12 -0400

    Initial commit

---
+++ test/rolie-hunter.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as RolieHunter from '../lib/rolie-hunter-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/rolie-hunter-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new RolieHunter.RolieHunterStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
