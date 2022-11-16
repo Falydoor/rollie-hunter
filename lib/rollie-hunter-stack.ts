156c4ed24d07e3f911ba8ed7fc96fddf82bc65a9
Theo LEBRUN <mib.ftw@gmail.com>
2022-11-16 16:09:59 -0500

    Add event bridge rule

--- lib/rollie-hunter-stack.ts
+++ lib/rollie-hunter-stack.ts
@@ -3,6 +3,8 @@ import { Construct } from 'constructs';
 import * as lambda from "aws-cdk-lib/aws-lambda";
 import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import * as iam from "aws-cdk-lib/aws-iam";
+import * as events from "aws-cdk-lib/aws-events";
+import * as targets from "aws-cdk-lib/aws-events-targets";
 
 export class RollieHunterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -37,5 +39,11 @@ export class RollieHunterStack extends cdk.Stack {
       resources: ['*'],
       effect: iam.Effect.ALLOW,
     }));
+
+    // Trigger lambda every 30 minutes
+    new events.Rule(this, 'Rule', {
+      schedule: events.Schedule.rate(cdk.Duration.minutes(30)),
+      targets: [new targets.LambdaFunction(hunterFunc)]
+    });
   }
 }
